2adb5d500ba7a4a2cb7818a07d0620c97748265c
Pratyush Kottawar <pratyushkottawar@gmail.com>
2024-01-08 08:43:52 +0530

    Made navbar responsic=ve

--- src/Create.js
+++ src/Create.js
@@ -1,7 +1,90 @@
+import { useState } from "react";
+
 const Create = () => {
+  const [thumbnail, setThumbnail] = useState("");
+  const [title, setTitle] = useState("");
+  const [publisher, setPublisher] = useState("");
+  const [author, setAuthor] = useState("");
+  const [publishingYear, setPublishingYear] = useState("");
+  const [eanCode, setEanCode] = useState("");
+  const [copiesInStock, setCopiesInStock] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const book = {
+      thumbnail,
+      title,
+      publisher,
+      author,
+      publishingYear,
+      eanCode,
+      copiesInStock,
+    };
+
+    fetch("https://mocki.io/v1/ee0353d4-0cb7-49f3-831a-6f18818a26e3", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(book),
+    }).then(() => {
+      console.log("new book added");
+    });
+  };
+
   return (
     <div className="create">
       <h2>Add new book</h2>
+      <form onSubmit={handleSubmit}>
+        <label>Thumbnail:</label>
+        <input
+          type="text"
+          required
+          value={thumbnail}
+          onChange={(e) => setThumbnail(e.target.value)}
+        />
+        <label>Title:</label>
+        <input
+          type="text"
+          required
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <label>Publisher:</label>
+        <input
+          type="text"
+          required
+          value={publisher}
+          onChange={(e) => setPublisher(e.target.value)}
+        />
+        <label>Author:</label>
+        <input
+          type="text"
+          required
+          value={author}
+          onChange={(e) => setAuthor(e.target.value)}
+        />
+        <label>Publishing Year:</label>
+        <input
+          type="text"
+          required
+          value={publishingYear}
+          onChange={(e) => setPublishingYear(e.target.value)}
+        />
+        <label>EAN Code:</label>
+        <input
+          type="text"
+          required
+          value={eanCode}
+          onChange={(e) => setEanCode(e.target.value)}
+        />
+        <label>Copies in Stock:</label>
+        <input
+          type="text"
+          required
+          value={copiesInStock}
+          onChange={(e) => setCopiesInStock(e.target.value)}
+        />
+        <button>Add Book</button>
+      </form>
     </div>
   );
 };
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,14 +1,60 @@
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
+  const showSidebar = () => {
+    const sidebar = document.querySelector(".sidebar");
+    sidebar.style.display = "flex";
+  };
+
+  const hideSidebar = () => {
+    const sidebar = document.querySelector(".sidebar");
+    sidebar.style.display = "none";
+  };
+
   return (
-    <nav className="navbar">
-      <h1>Product Management Dashboard</h1>
-      <div className="links">
-        <Link to="/"> Home </Link>
-        <Link to="/create">Add New Book</Link>
-      </div>
-    </nav>
+    <div>
+      <nav className="navbar">
+        <h1 className="navTitle">Product Management Dashboard</h1>
+        <div className="links">
+          <Link className="hideOnMobile" to="/">
+            {" "}
+            Home{" "}
+          </Link>
+          <Link className="hideOnMobile" to="/create">
+            Add Book
+          </Link>
+          <span className="hideOnDesktop" onClick={showSidebar}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              height="24"
+              viewBox="0 -960 960 960"
+              width="24"
+              style={{ position: "absolute" }}
+            >
+              <path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z" />
+            </svg>
+          </span>
+        </div>
+      </nav>
+      <nav className="sidebar">
+        <div onClick={hideSidebar}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            height="24"
+            viewBox="0 -960 960 960"
+            width="24"
+          >
+            <path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z" />
+          </svg>
+        </div>
+        <div className="sidelink">
+          <Link to="/"> Home </Link>
+        </div>
+        <div className="sidelink">
+          <Link to="/create">Add Book</Link>
+        </div>
+      </nav>
+    </div>
   );
 };
 
